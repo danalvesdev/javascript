b83461de4c890b427a7fe48f56abb3a944fb50c6
Daniel Alves <da445689@gmail.com>
2026-03-17 17:45:58 -0300

    desafio02

---
+++ aula012ex/ex014/script.js
@@ -0,0 +1,14 @@
+function carregar() {
+var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora > 0 && hora < 12){
+    img.src = 'fotomanha.jpg'
+    }else if(hora >= 12 && hora < 18){
+        img.src = 'fototarde.jpg'
+    }else{
+        img.src = 'fotonoite.jpg'
+    }
+}
