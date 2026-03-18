71ab591d2dbe91d92542011c9c5e3c78bbc73ad0
Daniel Alves <da445689@gmail.com>
2026-03-17 21:22:22 -0300

    ex014 concluido

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -1,14 +1,18 @@
-function carregar() {
-var msg = document.getElementById('msg')
-var img = document.getElementById('imagem')
-var data = new Date()
-var hora = data.getHours()
-msg.innerHTML = `Agora são ${hora} horas.`
-    if(hora > 0 && hora < 12){
-    img.src = 'fotomanha.jpg'
+function carregar(){
+    var msg = document.getElementById('mensagem')
+    var agora = new Date()
+    var hora = agora.getHours()
+    msg.innerHTML = (`Agora são ${hora} horas.`)
+    var img = document.getElementById('imagem')
+    
+    if(hora >= 0 && hora < 12){
+        img.src = 'img/fotomanha.jpg'
+        document.body.style.background = '#e2cd9f'
     }else if(hora >= 12 && hora < 18){
-        img.src = 'fototarde.jpg'
+        img.src = 'img/fototarde.jpg'
+        document.body.style.background = '#b9846f'
     }else{
-        img.src = 'fotonoite.jpg'
+        img.src = 'img/fotonoite.jpg'
+        document.body.style.background = '#515154'
     }
 }
