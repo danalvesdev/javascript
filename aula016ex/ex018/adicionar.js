8b833b8676776f6093ce5af50a2e43c34b13649b
Daniel Alves <da445689@gmail.com>
2026-03-26 00:49:31 -0300

    ex018 quase pronto

--- aula016ex/ex018/adicionar.js
+++ aula016ex/ex018/adicionar.js
@@ -1,9 +1,8 @@
 var numeros = []
+var campo = document.getElementById('num')
+var res = document.getElementById('resul')
 function clicar(){
-    var campo = document.getElementById('num')
-    var res = document.getElementById('resul')
-    var item = document.createElement('option')
-    var num = campo.value
+    var num = Number(campo.value)
     if(campo.value.length == 0 || numeros.indexOf(num) != -1){
         alert("Digite algo ou número diferente")
         return
@@ -13,12 +12,36 @@ function clicar(){
             return
         }
     }
+    var item = document.createElement('option')
     numeros.push(num)
     item.value = num
     item.innerHTML = `Você adicionou ${num}`
-    res.appendChild(item)
+    res.appendChild(item)    
+}
 
-    function calcular(){
-        console.log(numeros)
+var resultado = document.getElementById('calculos')
+function calcular(){
+    var maior = numeros[0]
+    var menor = numeros[0]
+    var soma = 0
+    var media = 0
+    for(let c = 0;c < numeros.length;c++){
+        let num = Number(numeros[c])
+        if(num > maior){
+            maior = num
+        }if(num < menor){
+            menor = num
+        }
+        soma = soma + num
+        media = soma / numeros.length
     }
+    resultado.innerHTML += `<h2>Ao todo temos ${numeros.length} números <br>`
+    resultado.innerHTML += `<h2>Número maior foi : ${maior} <br>`
+    resultado.innerHTML += `<h2>Número menor foi : ${menor} <br>`
+    resultado.innerHTML += `<h2>A soma dos números é : ${soma} <br>`
+    resultado.innerHTML += `<h2>A média dos valores é : ${media} <br>`
+
+    numeros = []
+    res.innerHTML = ''
+    resultado.innerHTML = ''
 }
