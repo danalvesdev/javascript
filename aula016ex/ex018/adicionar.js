bf24edcee57b5448a5acf0265a05d923bd99de78
Daniel Alves <da445689@gmail.com>
2026-03-27 17:36:26 -0300

    ex018 completo e um pouco de POO

--- aula016ex/ex018/adicionar.js
+++ aula016ex/ex018/adicionar.js
@@ -3,10 +3,15 @@ var campo = document.getElementById('num')
 var res = document.getElementById('resul')
 function clicar(){
     var num = Number(campo.value)
-    if(campo.value.length == 0 || numeros.indexOf(num) != -1){
-        alert("Digite algo ou número diferente")
+    if(campo.value.length == 0){
+        alert("Digite algo.")
         return
-    }else{
+    
+    }if(numeros.indexOf(num) != -1){
+        alert("Digite um número que não está na lista!")
+        return
+    }
+    else{
         if(num > 100  || num < 1){
             alert('Número inválido!')
             return
@@ -15,33 +20,41 @@ function clicar(){
     var item = document.createElement('option')
     numeros.push(num)
     item.value = num
-    item.innerHTML = `Você adicionou ${num}`
-    res.appendChild(item)    
+    item.innerHTML = `Valor ${num} adicionado`
+    res.appendChild(item)
+    resultado.innerHTML = ''  
+    campo.value = ''  
+    campo.focus()
 }
 
 var resultado = document.getElementById('calculos')
 function calcular(){
-    var maior = numeros[0]
-    var menor = numeros[0]
-    var soma = 0
-    var media = 0
-    for(let c = 0;c < numeros.length;c++){
-        let num = Number(numeros[c])
-        if(num > maior){
-            maior = num
-        }if(num < menor){
-            menor = num
+    if(numeros.length == 0){
+        alert("Adicione valores!")
+        }else{
+        var maior = numeros[0]
+        var menor = numeros[0]
+        var soma = 0
+        var media = 0
+        for(let c = 0;c < numeros.length;c++){
+            let num = Number(numeros[c])
+            if(num > maior){
+                maior = num
+            }if(num < menor){
+                menor = num
+            }
+            soma = soma + num
+            media = soma / numeros.length
         }
-        soma = soma + num
-        media = soma / numeros.length
+        if(numeros.length > 1){
+            resultado.innerHTML += `Ao todo temos ${numeros.length} números. <br>`
+        }else{
+            resultado.innerHTML += `Ao todo temos ${numeros.length} número. <br>`
+        }
+        res.innerHTML = ''
+        resultado.innerHTML += `Número maior foi : ${maior} <br>`
+        resultado.innerHTML += `Número menor foi : ${menor} <br>`
+        resultado.innerHTML += `A soma dos números é : ${soma} <br>`
+        resultado.innerHTML += `A média dos valores é : ${media} <br>`
     }
-    resultado.innerHTML += `<h2>Ao todo temos ${numeros.length} números <br>`
-    resultado.innerHTML += `<h2>Número maior foi : ${maior} <br>`
-    resultado.innerHTML += `<h2>Número menor foi : ${menor} <br>`
-    resultado.innerHTML += `<h2>A soma dos números é : ${soma} <br>`
-    resultado.innerHTML += `<h2>A média dos valores é : ${media} <br>`
-
-    numeros = []
-    res.innerHTML = ''
-    resultado.innerHTML = ''
 }
