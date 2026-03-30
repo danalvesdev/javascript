232d26cb93d8c6c8dddacf3ed341e3114186b2c6
Daniel Alves <da445689@gmail.com>
2026-03-30 16:40:29 -0300

    Create index.js

---
+++ aula018 novo curso/index.js
@@ -0,0 +1,8 @@
+let num1 = 9;
+
+//Math.floor() - Arredonda para baixo
+//Math.ceil() - Arredonda para cima
+//Math.round() - Arredonda para o mais próximo
+//Math.Max() - Retorna o maior número   
+//Math.Min() - Retorna o menor número
+//Math.random() - Retorna um número aleatório entre 0 e 1
