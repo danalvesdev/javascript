a12a341266fab5c58c0400bcfb70ead948702271
Daniel Alves <da445689@gmail.com>
2026-04-02 16:27:10 -0300

    aulas sobre operação ternária e objeto Date

---
+++ aula022 operação-ternária/index.js
@@ -0,0 +1,9 @@
+let idade = 17;
+
+let nivel = idade >= 18 ? "Você já é de maior" : "Você é de menor"
+console.log(`Sua idade : ${nivel}`)
+//if (usuario >= 1000){
+  //  console.log("Usuário VIP")
+//}else{
+   // console.log("Usuário comum")
+//}
---
+++ aula023 Objeto-Date/index.js
@@ -0,0 +1,5 @@
+const data = new Date()
+console.log("Agora são " + data.getHours() + " horas.")
+
+let data2 = new Date('2025-11-5 6:50')
+console.log(`Eu fiz uma cirurgia esse ano ${data}`)
