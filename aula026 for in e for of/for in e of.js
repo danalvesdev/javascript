1cabce7500699a9bcf197ca5ecbf244a681cce16
Daniel Alves <da445689@gmail.com>
2026-04-07 16:02:08 -0300

    for in,of e Each

---
+++ aula026 for in e for of/for in e of.js
@@ -0,0 +1,11 @@
+//Esse for so mostra os indices de arrays e chave de objetos
+const numeros = [1, 10, 29, 22]
+for(let i in numeros){
+    console.log(numeros)
+}
+
+//esse for of mostra os valores diretamente,melhor de usar em arrays
+let numeros02 = [100, 233, 999]
+for (let valor of numeros02){
+    console.log(valor)
+}
---
+++ aula026 for in e for of/forEach.js
@@ -0,0 +1,5 @@
+let numeros = [10, 20, 30]
+// Serve para percorrer e fazer algo, tipo mostrar dados, imprimir, etc.
+numeros.forEach(function(valor, ind){
+    console.log(valor , ind)
+})
