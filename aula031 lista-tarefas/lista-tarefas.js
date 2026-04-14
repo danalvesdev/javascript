3cfa41dda2d65ac5922b3b396e49b6b271bdbe2f
Daniel Alves <da445689@gmail.com>
2026-04-14 11:56:51 -0300

    aula031 lista de tarefas

---
+++ aula031 lista-tarefas/lista-tarefas.js
@@ -0,0 +1,22 @@
+const adicionar = document.getElementById('adicionar')
+const botao = document.getElementById('botao')
+let mostrar = document.getElementById('folha')
+
+botao.addEventListener('click', function(){
+    if(adicionar.value.length == 0){
+        alert('Adicione algo na sua lista!')
+        return
+    }
+    mostrar.innerHTML += `<ul><li>${adicionar.value} <button onclick="apagar(this)">apagar</button></li></ul>` 
+    localStorage.setItem('tarefas', mostrar.innerHTML)
+    localStorage.getItem('tarefas')
+});
+
+window.onload = function(){
+    mostrar.innerHTML = localStorage.getItem('tarefas')    
+}
+
+function apagar(botao){
+    botao.parentElement.remove()
+    localStorage.setItem('tarefas', mostrar.innerHTML)
+}
