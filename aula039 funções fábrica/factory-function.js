67b87ff75d30d60d9b3599681f5c9e332bf427eb
Daniel Alves <da445689@gmail.com>
2026-04-23 15:49:50 -0300

    Update factory-function.js

--- aula039 funções fábrica/factory-function.js
+++ aula039 funções fábrica/factory-function.js
@@ -2,8 +2,14 @@ function pessoa(nome, sobrenome, altura,peso){
     return {
         nome,
         sobrenome,
-        fala:(assunto) {
-            return `${this.peso} está ${assunto}`;
+        fala (assunto) {
+            return `${this.nome} ${this.sobrenome} está ${assunto}`;
+        },
+        altura: altura, 
+        peso: peso,
+        imc() {
+            const gordura = this.peso / (this.altura ** 2)
+            return gordura.toFixed(2)
         }
     }
 }
