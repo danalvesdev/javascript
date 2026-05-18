3e349472ce8b6f3e54c8d4700aea73ea6b939d9d
Daniel Alves <da445689@gmail.com>
2026-05-18 15:52:31 -0300

    Funções geradoras

--- ex044-funções-geradoras/main.js
+++ ex044-funções-geradoras/main.js
@@ -7,3 +7,37 @@ function* geradora1(){
 const g1 = geradora1()
 console.log(g1.next().value)
 console.log(g1.next().value)
+
+function* geradora2(){
+    let i = 1
+
+    while(true){
+        yield i;
+        i++
+    }
+}
+
+const g2 = geradora2()
+console.log(g2.next().value)
+console.log(g2.next().value)
+console.log(g2.next().value)
+console.log(g2.next().value)
+console.log(g2.next().value)
+
+function* geradora3(){
+    yield 0;
+    yield 1;
+    yield 2;
+}
+
+function* geradora4(){
+    yield* geradora3()
+    yield 3;
+    yield 4;
+    yield 5;
+}
+
+const g4 = geradora4()
+for(let valor of g4){
+    console.log(valor)
+}
