79acd6fd8ea7a4d09ae45400df1c8ed34453abfc
Daniel Alves <da445689@gmail.com>
2026-05-20 16:33:58 -0300

    revisão de array

---
+++ aula045-coisas-novas-arrays/main.js
@@ -0,0 +1,11 @@
+const nomes = ['João','Dan','Bella']
+delete nomes[1]
+console.log(nomes)
+
+const lista = ['Bella', 'Geruza', 'Reginaldo']
+const listaNova = lista.slice(0,3)// So pega os elementos ate onde o indice que eu colocar.
+console.log(listaNova)
+
+const nome = 'Daniel Alves Leite Pereira';
+const nomesDif = nome.split(' '); // Divide uma string em um array baseado em separador
+console.log(nomesDif)
